6fc4a1eb217cefc9180d29670473fd0f87605d00
BradJKim <bradjkim03@gmail.com>
2024-05-24 14:06:21 -0700

    implemented page loader for single responsibility

--- src/index.js
+++ src/index.js
@@ -2,68 +2,71 @@ import About from "./components/about/page";
 import Home from "./components/home/page";
 import Menu from "./components/menu/page";
 
-function App(){
+const pageLoader = (function () {
+    let currentPage = "home";
+
+    // maps page names to matching page.js
+    const pagesMap = {
+        "about": About(),
+        "home": Home(),
+        "menu": Menu(),
+    }
 
     // Function to remove current page content
     function cleanDocumentPage(){
         const page = document.querySelector(".page");
         const pageContent = document.querySelector(".pageContent");
         page.removeChild(pageContent);
-    }
+    } 
 
     // Function to load new page as page content
     function loadPage(pageName){
         const page = document.querySelector(".page");
-     
-        if(pageName === "about"){
-            const currentPage = About();
-            currentPage.className="pageContent";
-            page.appendChild(currentPage);
-        }
-        else if(pageName === "menu"){
-            const currentPage = Menu();
-            currentPage.className="pageContent";
-            page.appendChild(currentPage);
-        }
-        else{
-            const currentPage = Home();
-            currentPage.className="pageContent";
-            page.appendChild(currentPage);
-        }
+        const nextPage = pagesMap[pageName];
 
+        if(nextPage != null){
+            cleanDocumentPage();
+            
+            nextPage.className="pageContent";
+            
+            page.appendChild(nextPage);
+            currentPage = pageName;
+        }
     }
+
+    return { loadPage };
+})();
+
+function App(){
+
+    // page & page content initial setup
+    const page = document.createElement('div');
+    page.className='page';
+
+    const pageContent = document.createElement('div');
+    pageContent.className='pageContent';
+    
+    page.appendChild(pageContent);
 
     // Button liseners to remove current page content and replace with new page
     const homeButton = document.querySelector('.homeButton');
     homeButton.addEventListener('click', () => {
-        cleanDocumentPage();
-        loadPage("home");
+        pageLoader.loadPage("home");
     });
 
     const aboutButton = document.querySelector('.aboutButton');
     aboutButton.addEventListener('click', () => {
-        cleanDocumentPage();
-        loadPage("about");
+        pageLoader.loadPage("about");
     });
 
     const menuButton = document.querySelector('.menuButton');
     menuButton.addEventListener('click', () => {
-        cleanDocumentPage();
-        loadPage("menu");
+        pageLoader.loadPage("menu");
     });
-
-    // Create initial page element,
-    // Set init page as home page
-    const page = document.createElement('div');
-    page.className="page";
-
-    const homePage = Home();
-    homePage.className="pageContent";
-
-    page.appendChild(homePage);
 
     return page;
 }
 
 const content = document.querySelector('#content')
 content.appendChild(App());
+pageLoader.loadPage("home");
