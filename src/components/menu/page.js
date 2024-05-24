0d44cbc97eaa7180cdb9b079153260298b94fc6a
BradJKim <bradjkim03@gmail.com>
2024-05-24 13:13:22 -0700

    update dev/prod webpack config, added styles/css loaders, added items to menu

--- src/components/menu/page.js
+++ src/components/menu/page.js
@@ -1,6 +1,52 @@
+import PizzaImage from './best-pizza-los-angeles-kyle-moreno.jpg'
+import './style.css'
+
 export default function Menu(){
     const element = document.createElement('div');
-    element.innerHTML = "menu page";
+    
+    // Create title
+    const title = document.createElement('h1');
+    title.innerHTML = "Current Available Menu";
+
+    element.appendChild(title);
+
+    // Creating menu with images (tmp images)
+    const menu = document.createElement('div'); 
+    menu.classList.add('menuBar');
+
+    const pizzaImage1 = new Image();
+    pizzaImage1.src = PizzaImage;
+    pizzaImage1.width = "200";
+    pizzaImage1.height = "200";
+    pizzaImage1.classList.add('menuItem');
+
+    menu.appendChild(pizzaImage1);
+
+    const pizzaImage2 = new Image();
+    pizzaImage2.src = PizzaImage;
+    pizzaImage2.width = "200";
+    pizzaImage2.height = "200";
+    pizzaImage2.classList.add('menuItem');
+
+    menu.appendChild(pizzaImage2);
+
+    const pizzaImage3 = new Image();
+    pizzaImage3.src = PizzaImage;
+    pizzaImage3.width = "200";
+    pizzaImage3.height = "200";
+    pizzaImage3.classList.add('menuItem');
+
+    menu.appendChild(pizzaImage3); 
+
+    const pizzaImage4 = new Image();
+    pizzaImage4.src = PizzaImage;
+    pizzaImage4.width = "200";
+    pizzaImage4.height = "200";
+    pizzaImage4.classList.add('menuItem');
+
+    menu.appendChild(pizzaImage4); 
+
+    element.appendChild(menu);
     
     return element;
 }
